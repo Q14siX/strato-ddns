0e2869fed45bd7ccb78013dd420ee684e1f07711
Stefan Meyer <meyer.stefan1@gmx.net>
2025-07-23 21:17:22 +0200

    Update strato-ddns-start.sh

--- strato-ddns-start.sh
+++ strato-ddns-start.sh
@@ -4,10 +4,12 @@ set -e
 # Bildschirm leeren
 clear
 
+REPO_BASE_URL="https://raw.githubusercontent.com/Q14siX/strato-ddns/main"
+
 if [ -d /opt/strato-ddns ]; then
     echo "📥 Lade aktuelles Menü von GitHub …"
 else
     echo "🚀 Installation wird gestartet …"
 fi
 
-bash <(wget -qO- "https://raw.githubusercontent.com/Q14siX/strato-ddns/main/scripts/menu.sh")
+bash <(wget -qO- "$REPO_BASE_URL/scripts/strato-ddns-menu.sh")
