8c8737d7d34db3a6bd8b137cb838ca4dee047e60
Stefan Meyer <meyer.stefan1@gmx.net>
2025-07-25 15:25:51 +0200

    Update strato-ddns-template-default-update.sh

--- templates/default/strato-ddns-template-default-update.sh
+++ templates/default/strato-ddns-template-default-update.sh
@@ -11,14 +11,20 @@ cat > "$APP_DIR/templates/update.html" <<'EOF_HTML'
 <body>
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="/">Strato DDNS</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"><span class="navbar-toggler-icon"></span></button>
+    <a class="navbar-brand" href="/log">Strato DDNS</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+      <span class="navbar-toggler-icon"></span>
+    </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav me-auto">
-        <li class="nav-item"><a class="nav-link" href="/log">Protokoll</a></li>
+        <li class="nav-item"><a class="nav-link active" href="/log">Protokoll</a></li>
         <li class="nav-item"><a class="nav-link" href="/config">Konfiguration</a></li>
       </ul>
-      <span class="navbar-text"><a href="/logout" class="btn btn-outline-light btn-sm">Logout</a></span>
+      <ul class="navbar-nav ms-auto">
+        <li class="nav-item">
+          <a class="nav-link" href="/logout">Logout</a>
+        </li>
+      </ul>
     </div>
   </div>
 </nav>
