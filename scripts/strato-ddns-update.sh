7723ec40d6897345865d52dfd20221a196d5af26
Stefan Meyer <meyer.stefan1@gmx.net>
2025-07-26 15:30:52 +0200

    Update strato-ddns-update.sh

--- scripts/strato-ddns-update.sh
+++ scripts/strato-ddns-update.sh
@@ -43,11 +43,11 @@ echo "== Python-Abhängigkeiten installieren =="
 pip3 install --break-system-packages openpyxl
 
 # ========== App einspielen ==========
-echo "🐍 Applikation wird installiert"
+echo "🐍 Neuste Version der Applikation wird installiert"
 wget -q -O "$APP_DIR/app.py" "$REPO_URL/scripts/strato-ddns-app.py"
 
 # ========== Templates einspielen ==========
-echo "📄 Template wird installiert"
+echo "📄 Neuste Version des Templates wird installiert"
 wget -q -O "$APP_DIR/templates/_header.html" "$REPO_URL/templates/default/_header.html"
 wget -q -O "$APP_DIR/templates/_layout.html" "$REPO_URL/templates/default/_layout.html"
 wget -q -O "$APP_DIR/templates/config.html" "$REPO_URL/templates/default/config.html"
