4a71bc61302087ebe081da1e4c8866f95a6baace
Stefan Meyer <meyer.stefan1@gmx.net>
2025-07-26 16:18:37 +0200

    Update strato-ddns-webupdate.sh

--- scripts/strato-ddns-webupdate.sh
+++ scripts/strato-ddns-webupdate.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 set -e
 
-# ========== App einspielen ==========
 echo "🐍 Neuste Version der Applikation wird aktualisiert."
 wget -q -O "$APP_DIR/app.py" "$REPO_URL/scripts/strato-ddns-app.py"
 
-# ========== Templates einspielen ==========
 echo "📄 Neuste Version des Templates wird aktualisiert."
 wget -q -O "$APP_DIR/templates/_header.html" "$REPO_URL/templates/default/_header.html"
 wget -q -O "$APP_DIR/templates/_layout.html" "$REPO_URL/templates/default/_layout.html"
@@ -13,3 +11,9 @@ wget -q -O "$APP_DIR/templates/config.html" "$REPO_URL/templates/default/config.
 wget -q -O "$APP_DIR/templates/log.html" "$REPO_URL/templates/default/log.html"
 wget -q -O "$APP_DIR/templates/login.html" "$REPO_URL/templates/default/login.html"
 wget -q -O "$APP_DIR/templates/webupdate.html" "$REPO_URL/templates/default/webupdate.html"
+                
+echo "🛠️ Service-Dienste werden neu gestartet."
+systemctl daemon-reload
+systemctl restart strato-ddns
+
+echo "🔄 Update erfolgreich abgeschlossen!"
